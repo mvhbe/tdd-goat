b42d1141c210e6dfd6db6dc0f733d72bdb874991
Guido Van Hoof <guido.van.hoof3@telenet.be>
2014-07-17 21:38:51 +0200

    add deliberately failing test

--- lists/tests.py
+++ lists/tests.py
@@ -1,3 +1,7 @@
 from django.test import TestCase
 
-# Create your tests here.
+class SmokeTest(TestCase):
+
+    def testBadMaths(self):
+        '''A smoke test'''
+        self.assertEqual(1 + 1, 3)
